0c283d356df996197936d93d9e6f913bd21cfe7b
helegra <e.b.g@mail.ru>
2022-12-07 23:38:43 +0300

    programm was added

--- Test_Gracheva/Program.cs
+++ Test_Gracheva/Program.cs
@@ -11,7 +11,36 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-string [] arrayInitial={AAAAAA, BBBBB, CCCC, DDD, EE, F}; // Инициализация массива строк
+string [] arrayInitial={"AAAAAA", "BBBBB", "CCCC", "DDD", "EE", "F"}; // Инициализация массива строк
 
+string [] ThreeLettersArray(string[] array)
+{
+    int count = 0;
+    //считаем кол-во строк с нужной длиной
+    for (int i=0; i<array.Length; i++) {
+        if (array[i].Length <= 3) {
+            count++;
+        }
+    }
+    string [] result = new string[count]; //Создаем новый массив нужной длины
+    int j = 0; //Индекс результирующего массива
+    for (int i=0; i<array.Length; i++) {
+        if (array[i].Length <= 3) {
+            result[j] = array[i];
+            j++;
+        }
+    }
+    return result;
+}
 
+void PrintArray(string[] array) 
+{
+    Console.Write(String.Join(", ", array));
+}
+
+Console.Write("Массив: [");
+PrintArray(arrayInitial);
+Console.Write("] -> [");
+PrintArray(ThreeLettersArray(arrayInitial));
+Console.WriteLine("]");
 
